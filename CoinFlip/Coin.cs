509f204c549fb8a16a03507924cf602b531d907f
GuilhermeRaphael <guigaorapha07@gmail.com>
2025-04-11 22:31:40 -0300

    Criado classe Coin

---
+++ CoinFlip/Coin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    //Classe internal especifica que a classe gerada é acessível somente dentro de tipos no mesmo assembly.
+    public class Coin
+    {
+        //ModificadorDeAcesso (TIPO) (NOME)
+        private string ladoSorteado; //um atributo que pode ser nulo, colocamos ? do lado dele 
+
+        public string LadoSorteado { get => ladoSorteado; set => ladoSorteado = value; } //L para usar o metodo get e set
+
+        //metodo costuma ser publico
+        public Coin() 
+        {
+            //metodo construtor, metodo com o mesmo nome da classe se torna o construtor 
+        }
+
+        public string Flip()
+        {
+            Random random = new Random();
+           
+           /* if (random.Next(2) == 0)
+            {
+                LadoSorteado = "Cara";
+            }
+            else
+            {
+                LadoSorteado = "Coroa";
+            }
+           */
+
+            //operador ternario so serve quando vou fazer uma atribuição no IF
+            LadoSorteado = random.Next(2) == 0 ? "Cara" : "Coroa";
+            return (LadoSorteado);
+        }
+    }
+}
--- CoinFlip/MainPage.xaml.cs
+++ CoinFlip/MainPage.xaml.cs
@@ -1,54 +1,52 @@
 ﻿using Microsoft.Maui.Controls.Platform;
 using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CoinFlip
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
 
         public MainPage()
         {
             InitializeComponent();
+
         }
 
 
         private async void FlipButton_Clicked(object sender, EventArgs e)
         {
-
             //Algoritmo
             //Verificar qual opcao esta selecionada
             //Fazer o sorteio da moeda  (Random.Next(2))
             //Exibir o lado que foi sorteado (alterar a propriedade, nome do objeto.classe) MoedaImage.Source="coroa.png"
             //Verificar se a opção é igual ao lado escolhido
             //Exibir o resultado do jogo em um alerta (DisplayAlert)
-            int selectedIndex = SelecaoPicker.SelectedIndex;
 
-            Random rnd = new Random();
+             Random rnd = new Random();
+             int sorteio = rnd.Next(2);
 
-            int sorteio = rnd.Next(2);
+             if (sorteio == 0)
+             {
+                 MoedaImg.Source = "cara.png";
+             }
+             else
+             {
+                 MoedaImg.Source = "coroa.png";
+             }
 
-            if (sorteio == 0)
-            {
-                MoedaImg.Source = "cara.png";
-            }
-            else
-            {
-                MoedaImg.Source = "coroa.png";
-            }
+             if (SelecaoPicker.SelectedIndex == sorteio )
+             {
+                 await DisplayAlert("Parabéns!", "Você ganhou!", "OK");
+             }
+             else
+             {
+                 await DisplayAlert("Que pena!", "Você perdeu. Tente novamente.", "OK");
+             }
+         }
 
-            if (selectedIndex == sorteio )
-            {
-                await DisplayAlert("Parabéns!", "Você ganhou!", "OK");
-            }
-            else
-            {
-                await DisplayAlert("Que pena!", "Você perdeu. Tente novamente.", "OK");
-            }
-
-
-
-
-        }
     }
+
 }
+
